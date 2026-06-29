7072a61956989705de865de6c11da72dff895d61
peymanazad <azad.peyman75@gmail.com>
2026-06-29 15:05:34 +0400

    find the task of the pid from write op and display the info

--- proc_kernel.c
+++ proc_kernel.c
@@ -1,15 +1,13 @@
-#include "linux/fs.h"
-#include "linux/printk.h"
-#include "linux/sched.h"
-#include <linux/init.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/pid.h>
 #include <linux/proc_fs.h>
+#include <linux/sched.h>
 #include <linux/uaccess.h>
 
 #define BUFFER_SIZE 128
 #define PROC_NAME "hello"
 
+static pid_t target_pid = 0;
 ssize_t pread(struct file *file, char __user *usr_buf, size_t count,
               loff_t *pos);
 ssize_t pwrite(struct file *, const char *, size_t, loff_t *);
@@ -30,11 +28,20 @@ void proc_exit(void) { remove_proc_entry(PROC_NAME, NULL); }
 ssize_t pread(struct file *file, char __user *usr_buf, size_t count,
               loff_t *pos) {
     char buffer[BUFFER_SIZE];
+    struct task_struct *task;
+    if (target_pid == 0)
+        task = current;
+    else
+        task = pid_task(find_vpid(target_pid), PIDTYPE_PID);
+
+    if (task == NULL) {
+        return -ESRCH;
+    }
 
     char task_name[TASK_COMM_LEN];
-    get_task_comm(task_name, current);
-    char task_stat_char = task_state_to_char(current);
-    int rv = sprintf(buffer, "pid: %d name: %s, state: %c\n", current->pid,
+    get_task_comm(task_name, task);
+    char task_stat_char = task_state_to_char(task);
+    int rv = sprintf(buffer, "pid: %d, name: %s, state: %c\n", task->pid,
                      task_name, task_stat_char);
 
     return simple_read_from_buffer(usr_buf, count, pos, buffer, rv);
@@ -61,6 +68,7 @@ ssize_t pwrite(struct file *file, const char __user *usr_buf, size_t count,
         return -EINVAL;
 
     printk(KERN_INFO "inspector: received PID %ld\n", pid);
+    target_pid = pid;
 
     return count; // must return count, not sscanf result
 }
